a278ebd368169c48e2191076904ad2989ec3297b
Ricardo <ric.dalvarez@hotmail.com>
2021-12-07 11:56:18 +0100

    need to: add billing acount api key

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
 							<div className={classes.searchIcon}>
 								<SearchIcon />
 							</div>
-							<InputBase placeHolder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}}/>
+							<InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}}/>
 						</div>
 					{/* </Autocomplete> */}
 				</Box>
--- app/src/components/Map/Map.jsx
+++ app/src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Divider, Paper, Typography, useMediaQuery } from "@material-ui/core"
-import LocationOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
 import Rating from "@material-ui/lab"
 
 import useStyles from "./styles"
@@ -12,17 +12,18 @@ const Map = () => {
 	const isMobile = useMediaQuery("(min-width: 600px)")
 
 	const coordinates = { lat: 0, lng: 0 };
-
+	const APIKEY = process.env.REACT_APP_GOOGLE_API_KEY;
 	return (
 		<div className={classes.mapContainer}>
+			
 			<GoogleMapReact
-				bootstapURLKeys={{ key: "" }}
+				bootstapURLKeys={{ APIKEY }}
 				defaultCenter={coordinates}
 				center={coordinates}
 				defaultZoom={14}
 				margin={[50, 50, 50, 50]}
-				options={ ""}
-				onChange={ ""}
+				options={""}
+				onChange={""}
 				onChildClick={""}
 			>
 				
